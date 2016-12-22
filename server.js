46b23571b60781bc8781e4f8ba168610ac4ffd01
omgimanerd <alvin.lin.dev@gmail.com>
2016-12-22 17:55:33 -0500

    Base files

---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies.
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
